dedb2cb77ef99672a02568f178e2725c946ccf09
aminat13 <aminatcoster@gmail.com>
2026-02-21 19:59:46 +0000

    Delete scripts/01_data_overview.R

--- scripts/01_data_overview.R
+++
@@ -1,8 +0,0 @@
-#import dataset
-protein_data<-read.csv("data/immune_checkpoint_expression.csv")
-
-#inspect structure 
-head(protein_data)
-tail(protein_data)
-colnames(protein_data)
-dim(protein_data)
